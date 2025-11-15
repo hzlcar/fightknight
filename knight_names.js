ce9425dfd1b4fab565cff725724d7167e478bc8e
Hazel Caruthers <cjcaruthers2@gmail.com>
2025-11-14 22:33:03 -0800

    Knight names
    - added a name to the enemy knights (will eventually be randomized)
    - drafting a list of knight first names and titles

---
+++ knight_names.js
@@ -0,0 +1,16 @@
+var firstNames = ["Charles", "Frederick", "Shmingle", "Reginald", "Harold", "Jonathan", "Frorg", "Bartholomew", "Percival", 
+    "Gilgamesh", "Theodore", "Francis", "Saint Patrick", "Nicholas", "Alexander", "Don", "Edward", "Williamsworth", "Ferguson", "Elizabeth", 
+    "Eleanor", "Gilbert", "Joan", "Gabriel", "Wren", "Lilith", "Sofia", "Mildred", "Ptolemy", "Nikolaj", "Hazel", "Idean", "Wirt", "Frodo", 
+    "Gloria", "Maurice", "Walter", "Theodora", "Eloise", "Bridget", "Elena", "Isabella", "Genevieve", "Agnes", "Astrid", "Agatha", "Sybil", 
+    "Albert", "Katherine", "Edith", "Millicent", "Corrin", "Madeline", "Odysseus", "Clemente", "Grace", "Lance", "Claudia", "Reinhardt"];
+
+var lastNames = ["the Consumer of Reality", "the Brave", "the Merciful", "the Wise", "the Unspoken", "the Woman", "the Indubitable", 
+    "the Inevitable", "the Stranger", "the Beautiful", "the Kind", "the Antiquated One", "the Blade of Night", "the Sentinel", "the Old", 
+    "the Ancient", "the Stinky", "the Unsanctioned", "the Pathetic", "the Impressive", "the Warmonger", "the Goth", "the Titan", "the Mythical",
+    "the Radiant", "of the Lakes", "the Brick Wall", ", Heir to the Throne", "of the Great Flame", "the Broke", "the Terrifying", "the True...", 
+    "the Musician", "the Destroyer", "the Forgotten", "the Awesome", "the Puny", ", Dragon Slayer", "the Demonborn", "the Roaring", "the Unholy", 
+    "the Crusader", "the Lightbringer", "the Eternal", "the Impatient", "the Sad One", "of Distant Lands", "the Human", "the Wild", "the Crasher",
+    "the Ruined", "the Unkempt", "the Devious"];
+
+var specialNames = ["Knight Light", "Dilf Lorf", "Green Lord", "Scourblade", "Ground Beef", "Fender Bender", "Yaoi", "Vert Wheeler", 
+    "Two Relic Timmy", "Mumbo Jumbo"];
